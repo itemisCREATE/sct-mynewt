3d605b1683d56684c8999b1c0c2787b70492dcda
andreas muelder <muelder@itemis.de>
2018-09-06 15:51:21 +0200

    fixed releng errors, added travis configuration

--- plugins/org.yakindu.sct.mynewt.generator.c/src/org/yakindu/sct/mynewt/generator/c/MyNewtActivator.java
+++ plugins/org.yakindu.sct.mynewt.generator.c/src/org/yakindu/sct/mynewt/generator/c/MyNewtActivator.java
@@ -11,18 +11,10 @@ public class MyNewtActivator implements BundleActivator {
 		return context;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		MyNewtActivator.context = bundleContext;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		MyNewtActivator.context = null;
 	}
